34ab15207fd5d4b7b5cbbf60452fbb415f061898
BaltiBlast <fougeray.florian@gmail.com>
2024-09-23 18:04:29 +0200

    add PORT in .env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 // local
 const router = require("./router.js");
@@ -13,6 +14,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(router);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Le broche tourne sur le port ${process.env.PORT}`);
 });
